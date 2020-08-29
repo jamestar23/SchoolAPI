43094f2e886e46aee8ace55dedf01495a4cd3920
James Libetario Estrella <estrella.james23@gmail.com>
2020-08-29 13:00:04 -0400

    Added CourseRepo and EnrollmentEntryRepo

---
+++ SchoolAPI/Controllers/CourseRepo.cs
@@ -0,0 +1,31 @@
+﻿using SchoolAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolAPI.Controllers
+{
+    public class CourseRepo : IRepo<Course>
+    {
+        public void Create(Course item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Course Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Course item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SchoolAPI/Controllers/EnrollmentEntryRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolAPI.Controllers
+{
+    public class EnrollmentEntryRepo : IRepo<EnrollmentEntryRepo>
+    {
+        public void Create(EnrollmentEntryRepo item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EnrollmentEntryRepo Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(EnrollmentEntryRepo item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
